3a2ba12ebb36d837a8e73218ee5a888e8214ec64
Sabbir Hossain Zihad <sabbirhossainzihad76@gmail.com>
2024-10-26 21:39:41 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+// first Task
+
+// Declare variables
+let a = 10;
+let b = 5;
+// Perform operations
+let addition = a+b;
+let substraction = a-b;
+let multiplication = a*b;
+let division = a/b;
+// Display results in the console
+console.log("Addition :" + addition);
+console.log("Substraction :" + substraction);
+console.log("Multiplication :" + substraction);
+console.log("Division :" + division);
+
+
+
+
+// Scond task
+
+// Declare a variable for Celsius and store a value
+celsius = 40;
+// Convert Celsius to Fahrenheit
+let fahrenheit = (celsius * 9/5) + 32;
+
+// Print the Fahrenheit equivalent
+console.log(celsius + "°C is equal to " + fahrenheit + "°F");
+
+
+
+
+ // Third Task
+
+ let number = prompt(" Enter your number");
+
+number = Number(number);
+
+if(number>0){
+  console.log("The number is positive");
+}else if(number<0){
+  console.log("The number is Negative ");
+}else{
+  console.log("The number is zero")
+}
+
+
+
+
+
+// Fourth Task
+
+let marks = prompt("Enter your marks");
+
+marks = Number(marks);
+
+if(marks>=90){
+  console.log("A");
+}else if(marks>=80){
+  console.log("B");
+}else if(marks>=70){
+  console.log("C");
+}else if(marks>=60){
+  console.log("D");
+}else{
+  console.log("F");
+}
+
+
+// Fifth Task
+
+let numbers= 7;
+
+if(numbers % 2 == 0){
+  console.log("Even")
+}else{
+  console.log("Odd")
+}
+
+
+
